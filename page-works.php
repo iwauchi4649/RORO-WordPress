e323e70cc4c62854dc009a2907649289696e669f
hikaru08 <ur.mh08@gmail.com>
2020-07-15 09:41:09 +0900

    Update page-works.php

--- page-works.php
+++ page-works.php
@@ -1,15 +1,8 @@
 <?php
-/**
- * Template Name: page-nosidebar
- *
- * This is the template that displays all pages by default.
- * Please note that this is the WordPress construct of pages and that
- * other "pages" on your WordPress site will use a different template.
- *
- * @package WordPress
- * @subpackage Twenty_Sixteen
- * @since Twenty Sixteen 1.0
- */
+/*
+Template Name: works
+*/
+?>
  
 get_header(); ?>
  
