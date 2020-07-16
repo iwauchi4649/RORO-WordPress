39b3e2bff41ece79b4ce9aa4b928ef478d1ac720
Syoma Iwauchi <syoma4645@gmail.com>
2020-07-16 10:05:29 +0900

    list-box-width:25%に変更

--- page-creator.php
+++ page-creator.php
@@ -15,31 +15,13 @@ Template Name: creator
         </div>
         <!-- box4個の全体 -->
         <div class="creator__container__introduction__content">
-          <!-- ulタグ1個で全体50%、つまり2人で1セット -->
           <ul class="creator__container__introduction__content__box">
-            <!-- liは1個100%が2個並び -->
+            <!-- liは1個25%が4個並び、レスポンシブ時は1個50%が2個並び -->
             <li class="creator__container__introduction__content__box__square">
               <!-- aタグにpaddingを持たせるためdisplay:blockを付与 -->
               <a href="<?php echo home_url('/member'); ?>" class="square__text">
                 <!-- position:absoluteで中央寄せ -->
                 <span class="square__text__span">Syoma</span>
-              </a>
-            </li>
-            <li class="creator__container__introduction__content__box__square">
-              <a href="<?php echo home_url('/member'); ?>" class="square__text">
-                <span class="square__text__span">Takuro</span>
-              </a>
-            </li>
-          </ul>
-          <ul class="creator__container__introduction__content__box">
-            <li class="creator__container__introduction__content__box__square">
-              <a href="<?php echo home_url('/member'); ?>" class="square__text">
-                <span class="square__text__span">Takuma</span>
-              </a>
-            </li>
-            <li class="creator__container__introduction__content__box__square">
-              <a href="<?php echo home_url('/member'); ?>" class="square__text">
-                <span class="square__text__span">Hikaru</span>
               </a>
             </li>
           </ul>
