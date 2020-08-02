1a54bfb3e9906414b7673a00fabe88025398376e
takuma10Mi <amaebilove@gmail.com>
2020-08-02 20:25:42 +0900

    topクリエイターok

--- template/top_creator.php
+++ template/top_creator.php
@@ -1,9 +1,13 @@
-<!-- 10人以上をどのように並べるかは今後の課題 -->
-<?php $posts = get_posts('numberposts=10&category_name=creator'); global $post; ?>
-  <ul class="img-area">
-    <?php if($posts): foreach($posts as $post): setup_postdata($post); ?>
-      <li>
-        <a href="<?php the_permalink() ?>"><img class="circle-img" src="<?php the_field('right-image'); ?>" alt="sample"></a>
-      </li>
+<?php $posts = get_posts('numberposts=4&category_name=creator'); global $post; ?>
+<div class="flex">
+  <?php if($posts): foreach($posts as $post): setup_postdata($post); ?>
+      <div class="flex-item">
+        <div class="image-trim">
+          <a href="<?php the_permalink() ?>"><img class="circle-img" src="<?php the_field('right-image'); ?>" alt="sample"></a>
+        </div>
+        <h2><?php the_title(); ?></h2>
+        <p><?php the_field('occupation'); ?></p>
+        <a href="<?php the_permalink() ?>" class="btn-vertical-border">クリエイターページへ</a>
+      </div>
     <?php endforeach; endif; ?>
-  </ul>
+</div>
