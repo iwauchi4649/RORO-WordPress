f82f036548514e916ddd88821a68aac8bd07597e
ARK0730 <tk.ark0730@gmail.com>
2020-08-09 01:44:58 +0900

    image幅調整完了

--- template/top_works.php
+++ template/top_works.php
@@ -5,20 +5,6 @@
     </h1>
   </div>
 </div>
-<div class="wk-index-module">
-  <div class="wk-toolbar-module wk-toolbar-module--one-line wk-toolbar-module--works-index">
-    <div class="wk-toolbar-section-module">
-      <span class="wk-toolbar-section-module__heading">category</span>
-      <div class="wk-toolbar-section-module__content">
-        <ul class="wk-toolbar-section-module__list">
-          <li class="wk-toolbar-section-module__list-item">
-            <a href="#" class="is-current js-toggle-dropdown-menu">all view</a>
-          </li>
-        </ul>
-      </div>
-    </div>
-  </div>
-</div>
 <?php $posts = get_posts('numberposts=5&category_name=works'); global $post; ?>
   <ul class="your-class">
     <?php if($posts): foreach($posts as $post): setup_postdata($post); ?>
