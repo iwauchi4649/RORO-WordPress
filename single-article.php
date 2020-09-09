dedb4ae9aed565de99a0c0c49ed87efc7cbf9eaa
Syoma Iwauchi <syoma4645@gmail.com>
2020-09-09 10:35:28 +0900

    Update single-article.php

--- single-article.php
+++ single-article.php
@@ -56,25 +56,29 @@ Template Post Type: post
   </div>
 <?php endif; ?>
 <?php endwhile; endif; ?>
-  <div class="article__container" style="background-color: #ffffff;">
-    <?php $posts = get_posts('numberposts=3&category_name=article'); global $post; ?>
-    <?php if($posts): foreach($posts as $post): setup_postdata($post); ?>
-    <div class="article__container__item">
-      <a href="<?php the_permalink() ?>">
-        <div class="article__container__item__up-box">
-        <?php if(has_post_thumbnail()): ?>
-          <img src="<?php the_post_thumbnail_url(); ?>">
-        <?php else: ?>
-          <img src="<?php echo get_template_directory_uri()?>/images/fiber_web.png">
-        <?php endif; ?>
+  <div class="select-blog-area" style="background-color: #ffffff;">
+    <body class="article">
+      <div class="article__container">
+        <?php $posts = get_posts('numberposts=3&category_name=article'); global $post; ?>
+        <?php if($posts): foreach($posts as $post): setup_postdata($post); ?>
+        <div class="article__container__item" style="margin-top: 35px;">
+          <a href="<?php the_permalink() ?>">
+            <div class="article__container__item__up-box">
+            <?php if(has_post_thumbnail()): ?>
+              <img src="<?php the_post_thumbnail_url(); ?>">
+            <?php else: ?>
+              <img src="<?php echo get_template_directory_uri()?>/images/fiber_web.png">
+            <?php endif; ?>
+            </div>
+            <div class="article__container__item__down-box">
+              <span><?php the_time('Y年m月d日'); ?></span>
+              <h3><?php the_title(); ?></h3>
+            </div>
+          </a>
         </div>
-        <div class="article__container__item__down-box">
-          <span><?php the_time('Y年m月d日'); ?></span>
-          <h3><?php the_title(); ?></h3>
-        </div>
-      </a>
-    </div>
-    <?php endforeach; endif; ?>
+        <?php endforeach; endif; ?>
+      </div>
+    </body>
   </div>
 </body>
 <?php get_footer(); ?>
