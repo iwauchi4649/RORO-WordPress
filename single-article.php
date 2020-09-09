3dd222caebae8f7b0954cabdceb3dffa6cfbe070
Syoma Iwauchi <syoma4645@gmail.com>
2020-09-09 10:11:58 +0900

    single-article update

--- single-article.php
+++ single-article.php
@@ -10,7 +10,7 @@ Template Post Type: post
 <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 <?php $post_objects = get_field('creator'); if( $post_objects ): ?>
   <!-- 記事部分全体 (背景色がグレー) -->
-  <div class="article-container">
+  <div class="single-article-container">
 
     <!-- 左側に表示される「いいね」「ストック」ボタン -->
     <div class="stickyMenu"></div>
@@ -56,5 +56,25 @@ Template Post Type: post
   </div>
 <?php endif; ?>
 <?php endwhile; endif; ?>
+  <div class="article__container" style="background-color: #ffffff;">
+    <?php $posts = get_posts('numberposts=3&category_name=article'); global $post; ?>
+    <?php if($posts): foreach($posts as $post): setup_postdata($post); ?>
+    <div class="article__container__item">
+      <a href="<?php the_permalink() ?>">
+        <div class="article__container__item__up-box">
+        <?php if(has_post_thumbnail()): ?>
+          <img src="<?php the_post_thumbnail_url(); ?>">
+        <?php else: ?>
+          <img src="<?php echo get_template_directory_uri()?>/images/fiber_web.png">
+        <?php endif; ?>
+        </div>
+        <div class="article__container__item__down-box">
+          <span><?php the_time('Y年m月d日'); ?></span>
+          <h3><?php the_title(); ?></h3>
+        </div>
+      </a>
+    </div>
+    <?php endforeach; endif; ?>
+  </div>
 </body>
 <?php get_footer(); ?>
