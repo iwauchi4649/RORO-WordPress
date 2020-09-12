75316e926af0fd4136885c576c4d668e8712488a
Syoma Iwauchi <syoma4645@gmail.com>
2020-09-12 14:24:34 +0900

    map create

--- footer.php
+++ footer.php
@@ -1,6 +1,33 @@
   <?php wp_footer(); ?>  
   <footer class="footer">
-    <div class="footer__top"></div>
+    <div class="footer__top">
+      <div class="footer__top__container">
+        <div class="footer__top__container__adress">
+          <img src="<?php echo get_template_directory_uri()?>/images/kitchen.jpg">
+          <div class="footer__top__container__adress__logo">
+            <p class="">
+              <a class="">
+                <img src="<?php echo get_template_directory_uri()?>/">
+              </a>
+            </p>
+          </div>
+          <div class="footer__top__container__adress__info">
+            <p>
+              〒550-0014
+              <br>
+              大阪府大阪市西区北堀江1丁目17−10 クレオ北堀江 302
+              <br>
+              (TEL) 06-6684-9654 (FAX) 06-6684-9754
+              <br>
+              (E-MAIL) info@ro-ro.org
+            </p>
+          </div>
+        </div>
+        <div class="footer__top__container__map">
+          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3281.224679468274!2d135.49257421517692!3d34.67427849214066!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6000e7050117f3dd%3A0x6689e74874c6d1ed!2z44CSNTUwLTAwMTQg5aSn6Ziq5bqc5aSn6Ziq5biC6KW_5Yy65YyX5aCA5rGf77yR5LiB55uu77yR77yX4oiS77yR77yQ!5e0!3m2!1sja!2sjp!4v1584023554123!5m2!1sja!2sjp" frameborder="0"></iframe>
+        </div>
+      </div>
+    </div>
     <div class="footer__bottom">
       <div class="footer__bottom__container">
         <ul class="footer__bottom__container__link"></ul>
