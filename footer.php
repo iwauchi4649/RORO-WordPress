090f01dbc5492a0e56f08cee7e6679f2bcf53c06
Syoma Iwauchi <syoma4645@gmail.com>
2020-09-12 12:50:47 +0900

    footer create total100%

--- footer.php
+++ footer.php
@@ -1,4 +1,17 @@
   <?php wp_footer(); ?>  
-  <footer><small>&copy; RORO.inc All rights reserved</small></footer>
+  <footer class="footer">
+    <div class="footer__top"></div>
+    <div class="footer__bottom">
+      <div class="footer__bottom__container">
+        <ul class="footer__bottom__container__link"></ul>
+        <p class="footer__bottom__container__copyright">
+          &copy; RORO.inc All rights reserved
+        </p>
+      </div>
+    </div>
+    <div class="footer__return">
+      <a id="return-top" class="footer__return__top" href="#wrap"></a>
+    </div>
+  </footer>
   </body>
 </html>
