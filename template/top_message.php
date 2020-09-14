aa9b6128e194f6ebb25f8bcfc25c917ca5b50c9d
Syoma Iwauchi <syoma4645@gmail.com>
2020-09-14 20:49:19 +0900

    logo create

--- template/top_message.php
+++ template/top_message.php
@@ -1,3 +1,16 @@
 <section class="message">
-
+  <div class="message__container">
+    <div class="common-area">
+      <h3 class="common-area__title">MESSAGE</h3>
+      <p class="common-area__desc">イベント・見学会や、新築情報、今手がけている物件にまつわる情報をお伝えします。最新情報をお見逃しなく！</p>
+    </div>
+    <div class="message__container__body">
+      <div class="message__container__body__ceo">
+        <p>
+          <img src="<?php echo get_template_directory_uri()?>/images/ceo.jpg" alt="代表取締役">
+        </p>
+      </div>
+      <div class="message__container__body__text"></div>
+    </div>
+  </div>
 </section>
