4751828d0c4e2ed60e337b3d05e3584ebbf1033b
Syoma Iwauchi <syoma4645@gmail.com>
2020-09-15 10:38:02 +0900

    contact create

--- template/top_contact.php
+++ template/top_contact.php
@@ -1,3 +1,33 @@
 <section class="contact">
-
+  <div class="contact__container">
+    <div class="common-area">
+      <h3 class="common-area__title">CONTACT</h3>
+      <p class="common-area__desc">イベント・見学会や、新築情報、今手がけている物件にまつわる情報をお伝えします。最新情報をお見逃しなく！</p>
+    </div>
+    <div class="Form">
+      <div class="Form__Item">
+        <p class="Form__Item__Label">
+          <span class="Form__Item__Label__Required">必須</span>会社名
+        </p>
+        <input type="text" class="Form__Item__Input" placeholder="例）株式会社令和">
+      </div>
+      <div class="Form__Item">
+        <p class="Form__Item__Label"><span class="Form__Item__Label__Required">必須</span>氏名</p>
+        <input type="text" class="Form__Item__Input" placeholder="例）山田太郎">
+      </div>
+      <div class="Form__Item">
+        <p class="Form__Item__Label"><span class="Form__Item__Label__Any">任意</span>電話番号</p>
+        <input type="text" class="Form__Item__Input" placeholder="例）000__0000__0000">
+      </div>
+      <div class="Form__Item">
+        <p class="Form__Item__Label"><span class="Form__Item__Label__Required">必須</span>メールアドレス</p>
+        <input type="email" class="Form__Item__Input" placeholder="例）example@gmail.com">
+      </div>
+      <div class="Form__Item">
+        <p class="Form__Item__Label isMsg"><span class="Form__Item__Label__Required">必須</span>お問い合わせ内容</p>
+        <textarea class="Form__Item__Textarea"></textarea>
+      </div>
+      <input type="submit" class="Form__Btn" value="送信する">
+    </div>
+  </div>
 </section>
