120321b8fc93cbc6a7a180a2cd4902c07b4c63f6
Syoma Iwauchi <syoma4645@gmail.com>
2020-09-15 14:48:14 +0900

    attention create total50%

--- template/top_contact.php
+++ template/top_contact.php
@@ -23,9 +23,72 @@
         <p class="Form__Item__Label"><span class="Form__Item__Label__Required">必須</span>メールアドレス</p>
         <input type="email" class="Form__Item__Input" placeholder="例）example@gmail.com">
       </div>
-      <div class="Form__Item">
+      <div class="Form__Item" style="border-bottom: 1px solid #ddd;">
         <p class="Form__Item__Label isMsg"><span class="Form__Item__Label__Required">必須</span>お問い合わせ内容</p>
         <textarea class="Form__Item__Textarea" placeholder="例）お問い合わせ内容を記入してください。"></textarea>
+      </div>
+      <div class="Form__attention">
+        <div class="Form__attention__text">
+        <span class="title">
+          プライバシーポリシー
+          </span>
+          <span class="discription">
+          株式会社天授(以下『当社』という)は、個人情報に関する法令の遵守、個人情報の適切な取り扱い及び管理を徹底し、大切な個人情報の保護につとめてまいります。
+          </span>
+          <span class="sub-title">
+          個人情報の収集および利用
+          </span>
+          <span class="discription">
+          個人情報を収集する際には利用目的をお知らせします。
+          <br>
+          個人情報は、その利用目的をお知らせしてご本人の同意が得られた上でご提供いただきます。ご提供いただいた個人情報は、お知らせした利用目的の範囲を超えて利用しません。
+          </span>
+          <span class="discription">
+          利用目的の範囲について
+          <br>
+          ・業務上のご連絡をする場合
+          <br>
+          ・当社が取り扱う商品及びサービスに関するご案内をする場合
+          <br>
+          ・お客様からのお問い合わせまたはご依頼等への対応をさせていただく場合
+          <br>
+          ・その他、お客様に事前にお知らせし、ご同意をいただいた目的の場合
+          </span>
+          <span class="sub-title">
+          個人情報の第三者への提供・開示・訂正・消去
+          </span>
+          <span class="discription">
+          当社は、お客様の同意なしに第三者へお客様の個人情報の提供は行いません。
+          <br>
+          但し個人情報に適用される法律、その他の規範により、当社が従うべき法令上の義務等の特別な事情がある場合は、この限りではありません。
+          <br>
+          個人情報の開示・訂正・提供範囲の変更や削除を本人から依頼された場合には、合理的な範囲ですみやかに対応します。
+          </span>
+          <span class="sub-title">
+          個人情報の安全管理措置
+          </span>
+          <span class="discription">
+          徹底した管理によって個人情報の正確性及び安全性を確保します。
+          <br>
+          また、ご提供いただいた個人情報の紛失、破壊、改ざん及び漏洩等の防止に努めます。
+          <br>
+          回収致しました物品に関しても、分別・適切な処理を行い、お客様の個人を特定する情報が残ることはございません。
+          <br>
+          個人情報を扱う業務を他の会社に委託する場合、個人情報を収集するときの承諾に基づく利用、提供、安全管理を守るように、委託先に対する適切な契約や指導・管理をします。
+          </span>
+          <span class="sub-title">
+          継続的な改善について
+          </span>
+          <span class="discription">
+          当社が保有する個人情報に関して適用される法令、規則を遵守するとともに、適切な運用が実施されるよう管理と必要な是正を行い、個人情報保護の取り組みを継続的に見直し改善実施いたします。
+          </span>
+        </div>
+      </div>
+      <div class="Form__check">
+        <label for="agree">
+        上記規約内容に同意する
+        </label>
+        <input class="agree" type="checkbox">
       </div>
       <input type="submit" class="Form__Btn" value="送信する">
     </div>
