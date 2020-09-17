127da3db1c8cb9c53e8b919fb78f2375ce1e1ba3
Syoma Iwauchi <syoma4645@gmail.com>
2020-09-17 16:14:32 +0900

    shutter total100%

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php get_header()?>
+<?php get_template_part('template/top_shutter');?>
 <?php get_template_part('template/top_business');?>
 <?php get_template_part('template/top_campany');?>
 <?php get_template_part('template/top_message');?>
