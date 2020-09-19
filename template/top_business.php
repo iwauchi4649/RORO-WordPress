fe2bbbac31e224fb0ed55dbc84e2172f422e6a1c
hikaru08 <ur.mh08@gmail.com>
2020-09-19 11:16:42 +0900

    add_category_title

--- template/top_business.php
+++ template/top_business.php
@@ -1,12 +1,20 @@
 <section id="business" class="business">
   <ul class="business__contents">
     <li class="business__contents__item">
+      <span class="business__contents__item__title">
+        ONEROOM
+      </span>
       <div class="business__contents__item__background">
-        <p class="business__contents__item__background__description">プロのインテリアデザイナーによるお住まいに抜群にマッチした内装や家具の配置で創るデザイン例をご紹介します。</p>
+        <p class="business__contents__item__background__description">
+          プロのインテリアデザイナーによるお住まいに抜群にマッチした内装や家具の配置で創るデザイン例をご紹介します。
+        </p>
       </div>
       <img src="<?php echo get_template_directory_uri()?>/images/building1.jpg" alt="">
     </li>
     <li class="business__contents__item">
+      <span class="business__contents__item__title">
+        TOWER
+      </span>
       <div class="business__contents__item__background">
         <p class="business__contents__item__background__description">お客様のイメージに沿って、コストパフォーマンスとバランスを取りつつ、理想的な住まいのカタチを提供します。</p>
       </div>
@@ -15,12 +23,18 @@
   </ul>
   <ul class="business__contents">
     <li class="business__contents__item">
+      <span class="business__contents__item__title">
+        APARTMENT
+      </span>
       <div class="business__contents__item__background">
         <p class="business__contents__item__background__description">新築でもない、中古でもない。MONOLITHはリノベーションという新しい住まい購入のあり方をご提案します。</p>
       </div>
       <img src="<?php echo get_template_directory_uri()?>/images/building3.jpg" alt="">
     </li>
     <li class="business__contents__item">
+      <span class="business__contents__item__title">
+        CONSULT
+      </span>
       <div class="business__contents__item__background">
         <p class="business__contents__item__background__description">私たちはモノや資源を大量に消費する暮らしと「豊かさ」の両⽴を考え、真に「豊かに住まう」という実現に向けて常に取り組んでいます。</p>
       </div>
