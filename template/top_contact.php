6f3eabb7d280a9e330299c96e818022f9cbbcf4d
Syoma Iwauchi <syoma4645@gmail.com>
2020-09-28 16:00:00 +0900

    Update top_contact.php

--- template/top_contact.php
+++ template/top_contact.php
@@ -1,4 +1,4 @@
 <!-- ↓デフォルトのpタグを自動で差し込まないようにする -->
 <?php remove_filter ('the_content', 'wpautop'); ?>
 <!-- ↓MW WP Formのフォーム識別子を差し込む -->
-<?php echo do_shortcode('[mwform_formkey key="5"]'); ?>
+<?php echo do_shortcode('[mwform_formkey key="7"]'); ?>
